7465848666200a5b55ef30bff065025720871c84
Bonvic <bundi.bonvic@gmail.com>
2022-10-09 09:59:28 +0300

    exlint autofix linting

--- src/migrations/1663646103055-week-6-c.ts
+++ src/migrations/1663646103055-week-6-c.ts
@@ -1,16 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class week6C1663646103055 implements MigrationInterface {
-    name = 'week6C1663646103055'
+  name = 'week6C1663646103055';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "space" DROP COLUMN "max_guests"`);
-        await queryRunner.query(`ALTER TABLE "space" ADD "max_guests" integer`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "space" DROP COLUMN "max_guests"`);
+    await queryRunner.query(`ALTER TABLE "space" ADD "max_guests" integer`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "space" DROP COLUMN "max_guests"`);
-        await queryRunner.query(`ALTER TABLE "space" ADD "max_guests" character varying`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "space" DROP COLUMN "max_guests"`);
+    await queryRunner.query(
+      `ALTER TABLE "space" ADD "max_guests" character varying`,
+    );
+  }
 }
