8c19df9538d53fc693fd3b2b7ebd3e5ffd4b4a27
Bonvic <bundi.bonvic@gmail.com>
2022-11-01 16:29:08 +0300

    change migrations scripts and seeding scripts

---
+++ src/config/seedsConfig.ts
@@ -0,0 +1,12 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import config from '.';
+
+export const connectionSource = new DataSource({
+  migrationsTableName: 'migrations',
+  type: 'postgres',
+  url: config().databaseUrl,
+  logging: true,
+  synchronize: false,
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/seeds/*.ts'],
+} as unknown as DataSourceOptions);
